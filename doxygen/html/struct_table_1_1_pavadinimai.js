c25f95086798149c5e5dcaffde4e0402bf31700d
KarolZivulskas <90027186+KarolZivulskas@users.noreply.github.com>
2022-06-04 11:09:19 +0300

    2.0 V dokumentacija
    Pridedu doxygen dokumentaciją.

---
+++ doxygen/html/struct_table_1_1_pavadinimai.js
@@ -0,0 +1,9 @@
+var struct_table_1_1_pavadinimai =
+[
+    [ "egzas", "struct_table_1_1_pavadinimai.html#ad987a4bcef430215d6dca81c9eadc0a1", null ],
+    [ "mediana", "struct_table_1_1_pavadinimai.html#a4b82958499ccf41b77b3f5cc78c1ba61", null ],
+    [ "namudarbai", "struct_table_1_1_pavadinimai.html#aa6cc61f0c27e6aceaec2069f5706b916", null ],
+    [ "pavarde", "struct_table_1_1_pavadinimai.html#a977421622ac415863bdece39bd10dda3", null ],
+    [ "vardas", "struct_table_1_1_pavadinimai.html#ad2e358549808d01d08122368fdaa3353", null ],
+    [ "vidurkis", "struct_table_1_1_pavadinimai.html#a6925865b12835c5ea8b7cc9d8dbf31cb", null ]
+];
